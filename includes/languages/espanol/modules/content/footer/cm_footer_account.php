87f4125dfcb7f290bd83c66ad9ce6c4ac4a0f021
S76483 <maymarah@everis.com>
2018-12-26 14:58:11 -0500

    cambios idioma español

---
+++ includes/languages/espanol/modules/content/footer/cm_footer_account.php
@@ -0,0 +1,28 @@
+<?php
+/*
+  Copyright (c) 2018, G Burton
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+  define('MODULE_CONTENT_FOOTER_ACCOUNT_TITLE', 'bloque de cuenta');
+  define('MODULE_CONTENT_FOOTER_ACCOUNT_DESCRIPTION', 'Agrega un bloque de cuenta al pie de página de tu sitio web');
+
+  define('MODULE_CONTENT_FOOTER_ACCOUNT_HEADING_TITLE', 'Servicios de empleado');
+
+  define('MODULE_CONTENT_FOOTER_ACCOUNT_BOX_ACCOUNT', 'Mi cuenta');
+  define('MODULE_CONTENT_FOOTER_ACCOUNT_BOX_ADDRESS_BOOK', 'Mi dirección');
+  define('MODULE_CONTENT_FOOTER_ACCOUNT_BOX_ORDER_HISTORY', 'Mi historial');
+  define('MODULE_CONTENT_FOOTER_ACCOUNT_BOX_LOGOFF', 'Apagado seguro');
+  define('MODULE_CONTENT_FOOTER_ACCOUNT_BOX_CREATE_ACCOUNT', 'Crear cuenta');
+  define('MODULE_CONTENT_FOOTER_ACCOUNT_BOX_LOGIN', 'Cliente existente? Inicia sesión aquí');
+
